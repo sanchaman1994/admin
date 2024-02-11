e147499e0f0cbd36445bb8a738b911499789a116
Sanchaman Subba <sanchamansubba16@gmail.com>
2024-02-11 22:14:39 +0530

    admin ui

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+
+const Search = ({ searchQuery }) => {
+    const eventUpdate = (e) => {
+        searchQuery(e);
+    };
+    return (
+        <div className="searchBar">
+            <input
+                placeholder="Search by name, email or role..."
+                onChange={eventUpdate}
+            />
+        </div>
+    );
+};
+
+export default Search;
